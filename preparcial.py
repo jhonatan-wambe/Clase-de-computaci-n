4a1b4283101f90365b99cc9fc355dc9bfdc1362f
jhonatan-wambe <jhonatan.gutierrez@urosario.edu.co>
2024-09-23 09:46:24 -0500

    Update preparcial.py

--- preparcial.py
+++ preparcial.py
@@ -1,17 +1,17 @@
-import random
+import random 
+#lista de equipos
+equipos = ["Millonarios", "Nacional", "America", "Junior", "Santa fe", "Tolima", "Bucaramanga", "Pasto", "Huila", "Alianza"]
 
-# Lista de equipos
-equipos = ["Nacional", "Millonarios", "América", "Junior", "Santa Fe"]
+# Funcion para simular un partido
 
-# Función para simular un partido
 def simular_partido(equipo1, equipo2):
-    goles1 = random.randint(0, 5)
-    goles2 = random.randint(0, 5)
+    goles1 = random.randint(0,5)
+    goles2 = random.randint(0,5)
     print(f"{equipo1} {goles1} - {goles2} {equipo2}")
-
+    
 # Elegir dos equipos al azar
-equipo_local = random.choice(equipos)
+equipo_local = random.choice(equipos) # random.choice(equipos) escoge un elemento aleatoriamente de la lista equipos 
 equipo_visita = random.choice([e for e in equipos if e != equipo_local])
 
-# Simular el partido
+# simular el partido
 simular_partido(equipo_local, equipo_visita)
